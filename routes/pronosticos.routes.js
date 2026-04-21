ed5def561785ed124f16827ed7e47f62564c998b
HECTOR LORENZO ARAGON NOGALES <hector.aragon@canadianschool.onmicrosoft.com>
2026-04-21 12:33:45 -0600

    fix guardar jornada endpoint export

--- routes/pronosticos.routes.js
+++ routes/pronosticos.routes.js
@@ -1,19 +1,10 @@
 const express = require("express");
-
 const router = express.Router();
 
 const controller = require("../controllers/pronosticos.controller");
 
 const authMiddleware = require("../middleware/auth.middleware");
 
-const validarJornadaAbierta = require("../middleware/jornada.middleware");
-
-router.post(
-    "/",
-    authMiddleware,
-    validarJornadaAbierta,
-    controller.guardarPronostico
-);
 
 router.post(
   "/guardar-jornada",
@@ -21,16 +12,12 @@ router.post(
   controller.guardarPronosticosJornada
 );
 
+
 router.get(
   "/usuario",
   authMiddleware,
   controller.obtenerPronosticosUsuario
 );
 
-router.get(
-  "/mis-pronosticos",
-  authMiddleware,
-  controller.obtenerPronosticosUsuario
-);
 
 module.exports = router;
